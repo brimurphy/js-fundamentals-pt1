3abaf17297ec231ae7fe0bb88e92fb013dd9686f
Brian <blpmurphy@gmail.com>
2021-03-31 20:47:06 +0100

    Basic value and variable fundamentals

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!!!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Brian");
+console.log(23);
+
+let firstName = "Emma";
+console.log(firstName + ' is ' + js);
+console.log(firstName + ' is ' + js);
+console.log(firstName + ' is ' + js);
+
+// Can't start with number
+// let 3years = 3;
+// Can't use keywords, add $ before reserved keyword for example
+let $new = 27;
+// Variable with value that won't change use capitals
+let PI = 3.1415;
+
+// Good
+let myFirstJob = "Electrician";
+let myCurrentJob = "Technician";
+// Bad
+let job1 = "electrician";
+let job2 = "technician";
+
+console.log(myFirstJob);
